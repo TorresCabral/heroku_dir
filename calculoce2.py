98c7f885ef4a01a4ba3ec8320542abf3f5c0a1c8
iohana <iohana@ufu.com>
2021-04-19 22:09:51 -0300

    tabela

---
+++ calculoce2.py
@@ -0,0 +1,25 @@
+from numpy import *
+
+#Declaração de constantes
+angA = 0
+angB = 120
+angC = -120
+carga = 100
+Van = carga*exp(1j*deg2rad(angA))
+Za = 15 + 0j
+iA = Van/Za
+Vbn = carga*exp(1j*deg2rad(angB))
+Zb = 10 + 5j
+iB = Vbn/Zb
+Vcn = carga*exp(1j*deg2rad(angC))
+Zc = 6 - 8j
+iC = Vbn/Zb
+iN = iA + iB + iC
+
+print(iA, iB, iC, iN)
+
+
+
+print(iC)
+
+
--- deploy.py
+++ deploy.py
@@ -4,7 +4,7 @@
 Created on Fri Sep 11 20:35:16 2020
 @author: Prof. Dr. Wellington M. S. Bernardes
 """
-
+from numpy import *
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -13,6 +13,7 @@ import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
 
 # Define variables
 myheading = 'Circuitos Trifásicos Desequilibrados (Configuração Estrela)'
@@ -45,123 +46,56 @@ app.layout = html.Div(children=[
 
     html.Div([
         dash_table.DataTable(
-            id='datatable_id',
-            data=dff.to_dict('records'),
-            columns=[
-                {"name": i, "id": i, "deletable": False, "selectable": False} for i in dff.columns
-            ],
-            editable=False,
-            filter_action="native",
-            sort_action="native",
-            sort_mode="multi",
-            row_selectable="multi",
-            row_deletable=False,
-            selected_rows=[],
-            page_action="native",
-            page_current= 0,
-            page_size= 6,
-            # page_action='none',
-            # style_cell={
-            # 'whiteSpace': 'normal'
-            # },
-            # fixed_rows={ 'headers': True, 'data': 0 },
-            # virtualization=False,
-            style_cell_conditional=[
-                {'if': {'column_id': 'countriesAndTerritories'},
-                    'width': '40%', 'textAlign': 'left'},
-                {'if': {'column_id': 'deaths'},
-                    'width': '30%', 'textAlign': 'left'},
-                {'if': {'column_id': 'cases'},
-                    'width': '30%', 'textAlign': 'left'},
-            ],
+            id='data_table',
+            columns=[{
+                'name': 'Column {}'.format(i),
+                'id': 'column-{}'.format(i),
+            } for i in range(1, 5)],
+            data=[
+            {'column-{}'.format(i): (j + (i-1)*5) for i in range(1, 5)}
+            for j in range(5)
+            ]
         ),
-    ],className='row'),
-
-    html.Div([
-        html.Div([
-            dcc.Dropdown(id='linedropdown',
-                options=[
-                         {'label': 'Deaths', 'value': 'deaths'},
-                         {'label': 'Cases', 'value': 'cases'}
-                ],
-                value='deaths',
-                multi=False,
-                clearable=False
-            ),
-        ],className='six columns'),
-
-        html.Div([
-        dcc.Dropdown(id='piedropdown',
-            options=[
-                     {'label': 'Deaths', 'value': 'deaths'},
-                     {'label': 'Cases', 'value': 'cases'}
-            ],
-            value='cases',
-            multi=False,
-            clearable=False
-        ),
-        ],className='six columns'),
-
-    ],className='row'),
-
-    html.Div([
-        html.Div([
-            dcc.Graph(id='linechart'),
-        ],className='six columns'),
-
-        html.Div([
-            dcc.Graph(id='piechart'),
-        ],className='six columns'),
-
-    ],className='row'),
-
-
-
-
- html.A('Acesse UFU',href='http://www.ufu.br'),
- html.Br(),
- html.A('Acesse Moodle', href=moodle_address)])
-
-#------------------------------------------------------------------
+        html.Div(id='output_div')
+    ])className='row'),
+])
 @app.callback(
-    [Output('piechart', 'figure'),
-     Output('linechart', 'figure')],
-    [Input('datatable_id', 'selected_rows'),
-     Input('piedropdown', 'value'),
-     Input('linedropdown', 'value')]
+    Output('output_div', 'children'),
+    Input('data_table', 'active_cell'),
+    State('data_table', 'data')
 )
-def update_data(chosen_rows,piedropval,linedropval):
-    if len(chosen_rows)==0:
-        df_filterd = dff[dff['countriesAndTerritories'].isin(['China','Iran','Spain','Italy'])]
-    else:
-        print(chosen_rows)
-        df_filterd = dff[dff.index.isin(chosen_rows)]
+def getActiveCell(active_cell, data):
+    if active_cell:
+        col = active_cell['column_id']
+        row = active_cell['row']
+        cellData = data[row][col]
+return html.P(f'row: {row}, col: {col}, value: {cellData}')
+return html.P('no cell selected')
 
-    pie_chart=px.pie(
-            data_frame=df_filterd,
-            names='countriesAndTerritories',
-            values=piedropval,
-            hole=.3,
-            labels={'countriesAndTerritories':'Countries'}
-            )
+dcc.Interval(             
+    id='interval-component',             
+    interval=1*1000, # in milliseconds             
+    n_intervals=0         
+)
+@app.callback(
+    Output('component-to-update', 'property'),                      
+    Input('interval-component', 'n_intervals')
+)
+def update(n_intervals):
+    if n_intervals > 0:
+        # update table data
 
+@app.callback(
+    Output('data_table', 'data'),
+    Input('interval-component', 'n_intervals'),
+    [State('data_table', 'data'), State('data_table', 'columns')]
+)
+def updateData(n, data, columns):
+    if n > 0:
+        data.append({c['id']: n for c in columns})
+    return data
+    
 
-    #extract list of chosen countries
-    list_chosen_countries=df_filterd['countriesAndTerritories'].tolist()
-    #filter original df according to chosen countries
-    #because original df has all the complete dates
-    df_line = df[df['countriesAndTerritories'].isin(list_chosen_countries)]
-
-    line_chart = px.line(
-            data_frame=df_line,
-            x='dateRep',
-            y=linedropval,
-            color='countriesAndTerritories',
-            labels={'countriesAndTerritories':'Countries', 'dateRep':'date'},
-            )
-    line_chart.update_layout(uirevision='foo')
-
-    return (pie_chart,line_chart)
 
 #------------------------------------------------------------------
 
