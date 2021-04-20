be3cf0689f30cd43b07b3cfce98746bc94a575ae
iohana <iohana@ufu.com>
2021-04-19 22:16:03 -0300

    tabela

--- calculoce2.py
+++ calculoce2.py
@@ -1,25 +1,26 @@
 from numpy import *
 
 #Declaração de constantes
-angA = 0
-angB = 120
-angC = -120
-carga = 100
-Van = carga*exp(1j*deg2rad(angA))
-Za = 15 + 0j
-iA = Van/Za
-Vbn = carga*exp(1j*deg2rad(angB))
-Zb = 10 + 5j
-iB = Vbn/Zb
-Vcn = carga*exp(1j*deg2rad(angC))
-Zc = 6 - 8j
-iC = Vbn/Zb
-iN = iA + iB + iC
+def calculo():
+    angA = 0
+    angB = 120
+    angC = -120
+    carga = 100
+    Van = carga*exp(1j*deg2rad(angA))
+    Za = 15 + 0j
+    iA = Van/Za
+    Vbn = carga*exp(1j*deg2rad(angB))
+    Zb = 10 + 5j
+    iB = Vbn/Zb
+    Vcn = carga*exp(1j*deg2rad(angC))
+    Zc = 6 - 8j
+    iC = Vbn/Zb
+    iN = iA + iB + iC
 
-print(iA, iB, iC, iN)
+    print(iA, iB, iC, iN)
 
 
 
-print(iC)
+    print(iC)
 
 
